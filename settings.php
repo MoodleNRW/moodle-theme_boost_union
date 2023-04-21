2691f45d81fc7ff7da1693bf2b47349fd8cc13f2
Diphylla <annika-lambert@t-online.de>
2023-04-21 10:55:10 +0000

    fix: db update to load new settings

--- settings.php
+++ settings.php
@@ -1673,7 +1673,7 @@ if ($hassiteconfig || has_capability('theme/boost_union:configure', context_syst
         
         // Setting: Toggle whether Slider is shown.
         $title = get_string('slideractivatedsetting', 'theme_boost_union', null, true);
-        $setting = new admin_setting_configcheckbox("theme_boost_union/slideractivated", $title, "", 0);
+        $setting = new admin_setting_configcheckbox("theme_boost_union/slideractivatedsetting", $title, "", 0);
         $tab->add($setting);
 
         // Setting: Toggle whether Slider Arrow Controls are shown.
