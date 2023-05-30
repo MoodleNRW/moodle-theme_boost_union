93ce2dd500177d7315cc3d874db5864fa555062d
Diphylla <annika-lambert@t-online.de>
2023-05-30 16:34:59 +0200

    only load slider when setting checked

--- layout/includes/slider.php
+++ layout/includes/slider.php
@@ -16,31 +16,32 @@ $generalsettings->showindicatornav = $config->{'sliderindicatornavsetting'};
 $generalsettings->showcaption = $config->{'slidercaptionsetting'};
 $generalsettings->showcontent = $config->{'slidercontentsetting'};
 
-switch ($config->{'slideranimationsetting'}) {
-    case 0:
-        $generalsettings->animation = "";
-    case 1:
-        $generalsettings->animation = "fade";
-    case 2:
-        $generalsettings->animation = "slide";
+if ($generalsettings->show) {
+    switch ($config->{'slideranimationsetting'}) {
+        case 0:
+            $generalsettings->animation = "";
+        case 1:
+            $generalsettings->animation = "fade";
+        case 2:
+            $generalsettings->animation = "slide";
+    }
+    $generalsettings->interval = $config->{'sliderintervalsetting'};
+    $templatecontext['slidergeneralsettings'] = $generalsettings;
+    
+    
+    $slides = array();
+    for ($i = 1; $i <= THEME_BOOST_UNION_SETTING_SLIDES_COUNT; $i++){
+        $slider_image = theme_boost_union_get_urlofsliderimage($i);
+    
+        $slider_content = new stdClass();
+        $slider_content->count = $i-1;
+        $slider_content->image = $slider_image;
+        $slider_content->imagetitle = $config->{'oneslideimagetitle'.$i};
+        $slider_content->link = $config->{'oneslidelink'.$i};
+        $slider_content->linktitle = $config->{'oneslidelinktitle'.$i};
+        $slider_content->caption = $config->{'oneslidecaption'.$i};
+        $slider_content->content = $config->{'oneslidecontent'.$i};
+        array_push($slides,$slider_content);
+    }
+    $templatecontext['slidecontent'] = $slides;
 }
-$generalsettings->interval = $config->{'sliderintervalsetting'};
-$templatecontext['slidergeneralsettings'] = $generalsettings;
-
-
-$slides = array();
-for ($i = 1; $i <= THEME_BOOST_UNION_SETTING_SLIDES_COUNT; $i++){
-    $slider_image = theme_boost_union_get_urlofsliderimage($i);
-
-    $slider_content = new stdClass();
-    $slider_content->count = $i-1;
-    $slider_content->image = $slider_image;
-    $slider_content->imagetitle = $config->{'oneslideimagetitle'.$i};
-    $slider_content->link = $config->{'oneslidelink'.$i};
-    $slider_content->linktitle = $config->{'oneslidelinktitle'.$i};
-    $slider_content->caption = $config->{'oneslidecaption'.$i};
-    $slider_content->content = $config->{'oneslidecontent'.$i};
-    array_push($slides,$slider_content);
-}
-$templatecontext['slidecontent'] = $slides;
-
