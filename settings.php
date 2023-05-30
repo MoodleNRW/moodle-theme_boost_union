da471929d96212124411c9e6a1b51c94f464c72d
Diphylla <annika-lambert@t-online.de>
2023-05-30 16:34:11 +0200

    unique names for slide settings

--- settings.php
+++ settings.php
@@ -1727,31 +1727,31 @@ if ($hassiteconfig || has_capability('theme/boost_union:configure', context_syst
 
             // Setting: Set title for picture.
             $name = 'theme_boost_union/oneslideimagetitle'.$i;
-            $title = get_string('oneslideimagetitle', 'theme_boost_union', null, true);
+            $title = get_string('oneslideimagetitle', 'theme_boost_union', array('no' => $i), null, true);
             $setting = new admin_setting_configtext($name, $title, "", "");
             $tab->add($setting);
 
             // Setting: Set link for slide.
             $name = 'theme_boost_union/oneslidelink'.$i;
-            $title = get_string('oneslidelink', 'theme_boost_union', null, true);
+            $title = get_string('oneslidelink', 'theme_boost_union', array('no' => $i), null, true);
             $setting = new admin_setting_configtext($name, $title, "", "");
             $tab->add($setting);
 
             // Setting: Set title for link.
             $name = 'theme_boost_union/oneslidelinktitle'.$i;
-            $title = get_string('oneslidelinktitle', 'theme_boost_union', null, true);
+            $title = get_string('oneslidelinktitle', 'theme_boost_union', array('no' => $i), null, true);
             $setting = new admin_setting_configtext($name, $title, "", "");
             $tab->add($setting);
 
             // Setting: Set Caption text.
             $name = 'theme_boost_union/oneslidecaption'.$i;
-            $title = get_string('oneslidecaption', 'theme_boost_union', null, true);
+            $title = get_string('oneslidecaption', 'theme_boost_union', array('no' => $i), null, true);
             $setting = new admin_setting_configtext($name, $title, "", "");
             $tab->add($setting);
             
             // Setting: Set Content text.
             $name = 'theme_boost_union/oneslidecontent'.$i;
-            $title = get_string('oneslidecontent', 'theme_boost_union', null, true);
+            $title = get_string('oneslidecontent', 'theme_boost_union', array('no' => $i), null, true);
             $setting = new admin_setting_configtext($name, $title, "", "");
             $tab->add($setting);
             
